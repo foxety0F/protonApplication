86706d3ef298e822d0176d5a5e6b6b7cdea16b07
foxety0F <59017965+foxety0F@users.noreply.github.com>
2020-08-11 11:43:15 +0300

    Update HelpDao.java
    Add information to HelpDao

--- ProtonProject/src/main/java/com/foxety0f/proton/modules/help/dao/HelpDao.java
+++ ProtonProject/src/main/java/com/foxety0f/proton/modules/help/dao/HelpDao.java
@@ -16,14 +16,34 @@ import com.foxety0f.proton.modules.ProtonEssences;
 import com.foxety0f.proton.modules.ProtonModules;
 import com.foxety0f.proton.modules.help.domain.HelpInformation;
 
+/**
+ * This DAO using for access to database and add/modify/delete helps on pages, reports and dashboards
+ * <br> Roles for access : ROLE_HELP_EDITOR
+ * <br> Roles for view : <>
+ * <br> Used in : HelpService
+ * <br> Relation to HelpController
+ */
 public class HelpDao extends AbstractDAO implements IHelpDao {
 
+	/**
+	 * Constructor HelpDao
+	 * @param DataSource dataSource - core dataSource from application.properties
+	 */
 	public HelpDao(DataSource dataSource) {
 		super(dataSource);
 	}
 
 	ProtonModules thisModule = ProtonModules.HELP;
 
+	/**
+	 * Method for create help on core link page, like Admin or Reports or into reports
+	 * @param String helpName - using for specify help name and viewed on bootbox title
+	 * @param String helpDescription - using for specify help description and it is informative in nature
+	 * @param String url - specify target url for shows help
+	 * @param Integer refTypeId - specify references type, getting from dictionary and using for differentiation of type page
+	 * @param String helpText - specify help content and save asis with html-tags
+	 * @param UserDetailsProton user - using for identify user create help
+	 */
 	public void createNewHelp(String helpName, String helpDescription, String url, Integer refTypeId, String helpText,
 			UserDetailsProton user) {
 		Map<String, Object> map = new HashedMap<String, Object>();
@@ -41,6 +61,16 @@ public class HelpDao extends AbstractDAO implements IHelpDao {
 		loggerWithUser(thisModule, map, ProtonEssences.INSERT, user);
 	}
 
+	/**
+	 * Using for change help
+	 * @param Integer helpId - unique help id from database, using for specify modified help
+	 * @param String helpName - using for specify help name and viewed on bootbox title
+	 * @param String helpDescription - using for specify help description and it is informative in nature
+	 * @param String url - specify target url for shows help
+	 * @param Integer refTypeId - specify references type, getting from dictionary and using for differentiation of type page
+	 * @param String helpText - specify help content and save asis with html-tags
+	 * @param UserDetailsProton user - using for identify user modify help
+	 */
 	public void updateHelp(Integer helpId, String helpName, String helpDescription, String url, Integer refTypeId,
 			String helpText, UserDetailsProton user) {
 		Map<String, Object> map = new HashedMap<String, Object>();
@@ -55,6 +85,11 @@ public class HelpDao extends AbstractDAO implements IHelpDao {
 				+ " help_text = :helpText" + " where id = :helpId", map);
 	}
 
+	/**
+	 * Support method for edit helpText without logging
+	 * @param Integer helpId - unique help id from database, using for specify modified help
+	 * @param String helpText - specify help content and save asis with html-tags
+	 */
 	public void updateHelp(Integer helpId, String helpText) {
 		Map<String, Object> map = new HashedMap<String, Object>();
 		map.put("helpId", helpId);
@@ -65,6 +100,12 @@ public class HelpDao extends AbstractDAO implements IHelpDao {
 		
 	}
 	
+	/**
+	 * Support method for edit helpText with logging
+	 * @param Integer helpId - unique help id from database, using for specify modified help
+	 * @param String helpText - specify help content and save asis with html-tags
+	 * @param UserDetailsProton user - using for identify user modify help
+	 */
 	public void updateHelp(Integer helpId, String helpText, UserDetailsProton user) {
 		Map<String, Object> map = new HashedMap<String, Object>();
 		map.put("helpId", helpId);
@@ -76,6 +117,11 @@ public class HelpDao extends AbstractDAO implements IHelpDao {
 		loggerWithUser(thisModule, map, ProtonEssences.UPDATE, user);
 	}
 
+	/**
+	 * Using for get help information (title, text) from url-identif
+	 * @param String requestUrl - string-value request url from controller
+	 * @return HelpInformation
+	 */
 	public HelpInformation getHelp(String requestUrl) {
 		Map<String, String> map = new HashedMap<String, String>();
 		map.put("url", requestUrl);
@@ -91,6 +137,11 @@ public class HelpDao extends AbstractDAO implements IHelpDao {
 		return null;
 	}
 
+	/**
+	 * Using for get help information (title, text) from id
+	 * @param Integer helpId - unique help Id from database
+	 * @return HelpInformation
+	 */
 	public HelpInformation getHelp(Integer helpId) {
 		Map<String, Integer> map = new HashedMap<String, Integer>();
 		map.put("helpId", helpId);
@@ -107,6 +158,11 @@ public class HelpDao extends AbstractDAO implements IHelpDao {
 		
 	}
 
+	/**
+	 * Using for get help information (title, text) from report-identif
+	 * @param Integer reportId - integer-value of report id
+	 * @return HelpInformation
+	 */
 	public HelpInformation getHelpReport(Integer reportId) {
 		Map<String, Integer> map = new HashedMap<String, Integer>();
 		map.put("reportId", reportId);
@@ -122,6 +178,11 @@ public class HelpDao extends AbstractDAO implements IHelpDao {
 		return null;
 	}
 
+	/**
+	 * Using for get help information (title, text) from construcor element (report/creator/ansible/dashboard)
+	 * @param Integer reportConstrId - integer-value constructor element
+	 * @return HelpInformation
+	 */
 	public HelpInformation getHelpConstructor(Integer reportConstrId) {
 		Map<String, Integer> map = new HashedMap<String, Integer>();
 		map.put("constrId", reportConstrId);
@@ -137,6 +198,10 @@ public class HelpDao extends AbstractDAO implements IHelpDao {
 		return null;
 	}
 	
+	/**
+	 * Using for get all helps
+	 * @return List<HelpInformation>
+	 */
 	public List<HelpInformation> getAllHelps() {
 		List<HelpInformation> result = querySource.query(
 				"select id helpId, help_name helpName, help_description helpDescription, request_url url, help_text helpText from proton_help_config",
